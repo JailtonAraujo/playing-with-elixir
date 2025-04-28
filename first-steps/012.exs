7830d65222a12a78f2831773a4a3be3107cf118a
JailtonAraujo <jailtonraneimoney@gmail.com>
2025-04-27 23:12:33 -0300

    improments in structs

--- first-steps/012.exs
+++ first-steps/012.exs
@@ -1,12 +1,12 @@
 defmodule User do
   defstruct name: "John", age: 27
 
-  def new do
-    %User{name: "Jailton", age: 22}
+  def new name, age do
+    %User{name: name, age: age}
   end
 end
 
-IO.puts(User.new().name)
+IO.puts(User.new("Jailton", 55).name)
 
 defmodule Main do
   def fill_user do
