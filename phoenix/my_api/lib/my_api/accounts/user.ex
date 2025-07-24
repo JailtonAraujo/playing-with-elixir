314a96d3e724c0d2cf1ff4bbb06b54a669d383e0
JailtonAraujo <jailtonraneimoney@gmail.com>
2025-07-24 00:07:56 -0300

    create rest api project

---
+++ phoenix/my_api/lib/my_api/accounts/user.ex
@@ -0,0 +1,20 @@
+defmodule MyApi.Accounts.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "users" do
+    field :name, :string
+    field :email, :string
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:name, :email])
+    |> validate_required([:name, :email])
+  end
+end
